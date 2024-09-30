89dedf2b0feadc47d9446d3595844afcceedfccb
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-09-30 16:21:15 +0300

    more styling

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const Array = [];
   for (const item of array) {
     Array.push(appendString + item);
