e4b0d720852fcfb6bf78006aeafc23c3b3627c2b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-03 16:20:12 +0300

    fix import path

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from '../node_modules/utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const obj = {};
