96c34892c49326b08d6c0487b006107d570ed805
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-03 18:04:13 +0300

    mplement a class named Currency:
        - Constructor attributes:
            code (String)
            name (String)
    method named displayFullCurrency that will return the attributes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (code) !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
