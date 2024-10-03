f1adee6b232025ce6dc3c7574f2d55fdd9e8b4bf
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-03 13:32:33 +0300

    collectively resolve all promises and log body firstName lastName to the console.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([
     uploadPhoto,
     createUser,
-  ]).then((body, firstName, lastName) => {
-    console.log(body, firstName, lastName);
-  }).catch(() => { console.log('Signup system offline'); });
+  ]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  }).catch(() => { console.log('Signup system offline', Error()); });
 }
