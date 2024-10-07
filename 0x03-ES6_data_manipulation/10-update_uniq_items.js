9c467bd11db6ca82a608329791ccc3facc84b33b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-07 21:48:14 +0300

    handle updating the quantity is not possible (argument is not a map)

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
 export default function updateUniqueItems(map) {
   return map.forEach((value, key) => {
     if (value === 1) {
-      map.set(key, 100);
+      try {
+        map.set(key, 100);
+      } catch (error) {
+        throw new Error('Cannot process');
+      }
     }
   });
 }
