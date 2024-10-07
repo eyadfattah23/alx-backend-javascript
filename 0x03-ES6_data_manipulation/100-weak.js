b5f4a4015d725b5415abbfb25c211c9b78c4651e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-07 22:38:31 +0300

    change count from outside the map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -7,9 +7,10 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0); // Initialize the counter if it doesn't exist
   }
-  const count = weakMap.get(endpoint);
+  let count = weakMap.get(endpoint);
+  count += 1;
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count);
 }
