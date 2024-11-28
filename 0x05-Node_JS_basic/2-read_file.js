0acbd3fb482241de6ea545321d2707a105b9c865
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-28 10:17:23 +0200

    create a function that reads from a csv file and prints it's data

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+
+  const file = fs.readFileSync(path, 'utf-8');
+
+  const students = file.split('\n').slice(1);
+  if (!students[-1]) { students.pop(); }
+
+  console.log(`Number of students: ${students.length}`);
+
+  const csStudents = [];
+  const SWEStudents = [];
+
+  for (const student of students) {
+    const std = student.split(',');
+    if (std[3] === 'CS') { csStudents.push(student.split(',')[0]); }
+    if (std[3] === 'SWE') { SWEStudents.push(student.split(',')[0]); }
+  }
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${SWEStudents.join(', ')}`);
+}
+
+module.exports = countStudents;
