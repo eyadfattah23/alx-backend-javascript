c504cfeb43c8cd794e4cd15e06434d402dbec4fc
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-28 12:31:15 +0200

    create a function that reads a db async and return a promise.

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+        return null;
+      }
+      const students = data.split('\n').slice(1);
+      if (!students[-1]) { students.pop(); }
+
+      console.log(`Number of students: ${students.length}`);
+      const csStudents = [];
+      const SWEStudents = [];
+
+      for (const student of students) {
+        const std = student.split(',');
+        if (std[3] === 'CS') { csStudents.push(student.split(',')[0]); }
+        if (std[3] === 'SWE') { SWEStudents.push(student.split(',')[0]); }
+      }
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+      console.log(`Number of students in SWE: ${SWEStudents.length}. List: ${SWEStudents.join(', ')}`);
+      resolve();
+      return null;
+    });
+  });
+}
+module.exports = countStudents;
